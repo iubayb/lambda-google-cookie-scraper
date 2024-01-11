3152ec0b3760636612cb832e24a614a9502a4e95
ayoub <ayoubrahmanii@gmail.com>
2024-01-11 10:50:22 +0400

    remove uncessary region declaration

--- src/libs/dynamoDb.ts
+++ src/libs/dynamoDb.ts
@@ -2,8 +2,4 @@
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
-const dynamoDbClient = new DynamoDBClient({
-  region: process.env.AWS_REGION as string,
-});
-
-export default DynamoDBDocument.from(dynamoDbClient);
+export default DynamoDBDocument.from(new DynamoDBClient());
